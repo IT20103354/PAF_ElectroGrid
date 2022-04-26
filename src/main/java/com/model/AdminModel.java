d092c74e90e6e5c4a7579411882c44af5cc85695
Niroshani A <nirosanju00@gmail.com>
2022-04-26 12:17:58 +0530

    Initial commit

---
+++ src/main/java/com/model/AdminModel.java
@@ -0,0 +1,79 @@
+package com.model;
+
+public class AdminModel {
+    private int adminID;
+    private String firstName;
+    private String lastName;
+    private String NIC;
+    private String email;
+    private String mobile;
+    private String department;
+    private String password;
+
+    public AdminModel() {
+    }
+
+    public int getAdminID() {
+        return adminID;
+    }
+
+    public void setAdminID(int adminID) {
+        this.adminID = adminID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getNIC() {
+        return NIC;
+    }
+
+    public void setNIC(String NIC) {
+        this.NIC = NIC;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
